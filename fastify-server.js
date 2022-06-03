66546a18816b3292dcf8eff0956dc5926f9958b2
JJSkillz2099-Star <105014770+JJSkillz2099-Star@users.noreply.github.com>
2022-06-03 15:52:29 -0700

    Add files via upload

---
+++ fastify-server.js
@@ -0,0 +1,98 @@
+let students = [
+    {
+      id: 1,
+      last: "Last1",
+      first: "First1",
+    },
+    {
+      id: 2,
+      last: "Last2",
+      first: "First2",
+    },
+    {
+      id: 3,
+      last: "Last3",
+      first: "First3",
+    }
+];
+
+const fastify = require("fastify")();
+// Handle GET verb for / route using Fastify
+// Note use of "chain" dot notation syntax
+fastify.get("/cit/student", (request, reply) => {
+  reply
+    .code(200)
+    .header("Content-Type", "application/json; charset=utf-8")
+    .send(students);
+});
+fastify.get("/cit/student/:id", (request, reply) => {
+  const { id } = request.params;
+  let student = null;
+  for (const item of students) {
+    if (item.id === parseInt(id)) {
+      student = item;
+      break;
+    }
+  }
+
+  if (student) {
+    reply
+      .code(200)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send(student);
+  } else {
+    if (!student) {
+      reply
+        .code(404)
+        .header("Content-Type", "text/html; charset=utf-8")
+        .send("Not found");
+    }
+  }
+  });
+  fastify.get("/cit/*", (request, reply) => {
+    reply
+      .code(404)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send(unmatched);
+  });
+
+fastify.post("/cit/student", (request, reply) => {
+  const { last, first } = request.body;
+  const id = null; 
+
+  if (!last || !first) {
+    reply
+      .code(404)
+      .header("Content-Type", "text/html; charset=utf-8")
+      .send("Not found");
+  } else {
+    let id = 0; 
+    for (const student of students) {
+      if(student.id > id) {
+        id = student.id; 
+      }
+    }
+    id++;
+    students.push({ id, last, first });
+
+    reply
+      .code(200)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send(students[students.length - 1]);
+  }
+  let response = request.body;
+  reply 
+    .code(200)
+    .header("Content-Type", "application/json; charset=utf-8")
+    .send(response);
+});
+
+const listenIP = "127.0.0.1";
+const listenPort = 8080;
+fastify.listen(listenPort, listenIP, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server listening on ${address}`);
+});
